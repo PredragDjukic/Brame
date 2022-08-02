bcac52a4dba829674d42db88f428b6151d452544
djukic.predrag <predrag.djukic@marbleit.rs>
2022-08-02 23:19:13 +0200

    Create campaign table

--- db scripts/brame.sql
+++ db scripts/brame.sql
@@ -4,3 +4,12 @@ create table users
     username varchar(100) not null,
     password varchar(255) not null
 )
+
+create table campaign
+(
+    Id int primary key,
+    name varchar(100) not null,
+    status int not null,
+    startDate timestamp null,
+    imagePath varchar(300) null
+)
