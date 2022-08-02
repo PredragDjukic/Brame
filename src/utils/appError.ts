bf86b927881aefd59829abfb4b0546a671894cde
Predrag Djukic <predrag.djukic@marbleit.rs>
2022-08-02 15:02:51 +0200

    Create custom error

---
+++ src/utils/appError.ts
@@ -0,0 +1,12 @@
+class AppError extends Error {
+    statusCode: number;
+  
+    constructor(message: string, statusCode: number = 500) {
+      super(message);
+  
+      this.statusCode = statusCode;
+    }
+  }
+  
+  export default AppError;
+  
