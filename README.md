2b890d664cf1436fc40bb78b8ae80535f953dbb2
Predrag Djukic <predrag.djukic@marbleit.rs>
2022-08-03 10:34:30 +0200

    Update db and readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Brame
+
+
+## Installation
+
+Use npm to install all packages
+
+```bash
+npm install
+```
+
+## Database
+
+Create a postgres database. All required data about database are stored in src/db.ts.
+
+Use the dbscripts/brame.sql to create tables and insert test data
+
+## Postman
+Import postmancollection/Brame.postman_collection.json into the Postman.
+
+Create 2 variables
+
+*apiBaseUrl* with value of http://localhost:8000/api
+
+*authToken* with initial random string. This one will be set up automatically after a sign up
+
--- db scripts/brame.sql
+++ db scripts/brame.sql
@@ -1,15 +1,32 @@
+drop table if exists users;
+drop table if exists campaign;
+
 create table users
 (
     Id int primary key,
     username varchar(100) not null,
     password varchar(255) not null
-)
+);
+
+insert into users
+values(1, 'pedja', '$2b$06$ROIksKJzJ2PHhceOJlW64.ivW8MQxXSmvg6IsPS6xerFeL2Op5DdW');
+insert into users
+values(2, 'nikola', '$2b$06$ROIksKJzJ2PHhceOJlW64.ivW8MQxXSmvg6IsPS6xerFeL2Op5DdW');
+insert into users
+values(3, 'jana', '$2b$06$ROIksKJzJ2PHhceOJlW64.ivW8MQxXSmvg6IsPS6xerFeL2Op5DdW');
 
 create table campaign
 (
-    Id int primary key identity,
+    Id serial primary key,
     name varchar(100) not null,
     status int not null,
     start_date timestamp null,
     image_path varchar(300) null
-)
+);
+
+insert into campaign(name, status, start_date, image_path)
+values ('Coca cola', 1, '2022-02-08 22:00:00', 'cocacola/imagepath');
+insert into campaign(name, status, start_date, image_path)
+values ('Plazma', 1, '2022-02-08 22:00:00', 'plazma/imagepath');
+insert into campaign(name, status, start_date, image_path)
+values ('Frolp,', 1, '2022-02-08 22:00:00', 'frikom/imagepath');
